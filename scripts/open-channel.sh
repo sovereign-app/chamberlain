c558628ccc8adb32c76b2900fa9b324688d09352
David Caseria <david@team.sovereign.app>
2024-09-26 07:57:33 -0400

    Fix docker compose and open channel script

--- scripts/open-channel.sh
+++ scripts/open-channel.sh
@@ -4,22 +4,22 @@ set -e
 shopt -s expand_aliases
 
 BITCOIN_CLI=${BITCOIN_CLI:-bitcoin-cli}
+CHAMBERLAIN=${CHAMBERLAIN:-chamberlain}
 
 NODE_ID=$1
 AMOUNT=$2
 BTC_AMOUNT=$(echo "scale=8; $AMOUNT / 100000000" | bc | awk '{printf "%.8f", $0}')
 
 # STEP 1: Open a channel with the node
-output=$(chamberlain open-channel --node-id $NODE_ID --amount $AMOUNT)
+output=$($CHAMBERLAIN open-channel --node-id $NODE_ID --amount $AMOUNT)
 channel_id=$(echo "$output" | awk '/channel id:/ {print $3}')
 address=$(echo "$output" | awk '/address:/ {print $2}')
-quote_id=$(echo "$output" | awk '/quote id:/ {print $3}')
 
 # STEP 2: Create funding transaction
 tx_hex=$($BITCOIN_CLI walletprocesspsbt $($BITCOIN_CLI walletcreatefundedpsbt [] "[{\"$address\":$BTC_AMOUNT}]" | jq -r '.psbt') | jq -r '.hex')
 
 # STEP 3: Fund channel
-output=$(chamberlain fund-channel --channel-id $channel_id --tx $tx_hex)
+output=$($CHAMBERLAIN fund-channel --channel-id $channel_id --tx $tx_hex)
 new_channel_id=$(echo "$output" | awk '/channel id:/ {print $3}')
 
 # STEP 4: Mine 3 blocks
@@ -31,4 +31,4 @@ do
 done
 
 # STEP 5: Issue channel token
-chamberlain issue-channel-token --channel-id $new_channel_id --quote-id $quote_id
+$CHAMBERLAIN issue-channel-token --channel-id $new_channel_id
